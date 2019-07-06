719e3740d0587843ad355e912357dac7831232da
Edward-1020 <iezzuyoung@gmail.com>
2019-07-07 00:36:59 +0800

    refactor: clear code

--- src/xhr.ts
+++ src/xhr.ts
@@ -1,44 +1,43 @@
-import { AxiosRequestConfig, AxiosPromise, AxiosResponse} from './types'
-import { resolve } from 'url';
+import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from './types'
 
 export default function xhr(config: AxiosRequestConfig): AxiosPromise {
-  return new Promise((resolve) => {
-      const { data = null, url, method = 'get', headers, responseType} = config;
+  return new Promise(resolve => {
+    const { data = null, url, method = 'get', headers, responseType } = config
 
-      const request = new XMLHttpRequest();
+    const request = new XMLHttpRequest()
 
-      if (responseType) {
-        request.responseType = responseType;
+    if (responseType) {
+      request.responseType = responseType
+    }
+
+    request.open(method.toUpperCase(), url, true)
+
+    request.onreadystatechange = function handleLoad() {
+      if (request.readyState !== 4) {
+        return
       }
 
-      request.open(method.toUpperCase(), url, true);
-
-      request.onreadystatechange = function handleLoad () {
-        if (request.readyState !== 4) {
-          return;
-        }
-
-        const responseHeaders = request.getAllResponseHeaders();
-        const responseData = responseType !== 'text' ? request.response : request.responseText;
-        const reponse: AxiosResponse = {
-          data: responseData,
-          status: request.status,
-          statusText: request.statusText,
-          headers: responseHeaders,
-          config,
-          request
-        }
-        resolve(reponse);
+      const responseHeaders = request.getAllResponseHeaders()
+      const responseData = responseType !== 'text' ? request.response : request.responseText
+      const reponse: AxiosResponse = {
+        data: responseData,
+        status: request.status,
+        statusText: request.statusText,
+        headers: responseHeaders,
+        config,
+        request
       }
+      resolve(reponse)
+    }
 
-      Object.keys(headers).forEach((name) => {
-        if (data === null && name.toLowerCase() === 'content-type') {
-          delete headers[name];
-        } else {
-          request.setRequestHeader(name, headers[name]);
-        }
-      });
+    Object.keys(headers).forEach(name => {
+      if (data === null && name.toLowerCase() === 'content-type') {
+        delete headers[name]
+      } else {
+        request.setRequestHeader(name, headers[name])
+      }
+    })
 
-      request.send(data);
-  });
+    request.send(data)
+  })
 }
