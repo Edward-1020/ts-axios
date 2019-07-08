1bae64924c7debfa4815fa8c9ab423c39b257367
Edward-1020 <iezzuyoung@gmail.com>
2019-07-08 23:24:39 +0800

    feat: handle error

--- src/xhr.ts
+++ src/xhr.ts
@@ -3,13 +3,17 @@ import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from './types'
 import { parseHeaders } from './helpers/headers';
 
 export default function xhr(config: AxiosRequestConfig): AxiosPromise {
-  return new Promise(resolve => {
-    const { data = null, url, method = 'get', headers, responseType } = config
+  return new Promise((resolve, reject) => {
+    const { data = null, url, method = 'get', headers, responseType, timeout } = config
 
     const request = new XMLHttpRequest()
 
     if (responseType) {
       request.responseType = responseType
+    }
+
+    if (timeout) {
+      request.timeout = timeout;
     }
 
     request.open(method.toUpperCase(), url, true)
@@ -19,9 +23,13 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
         return
       }
 
+      if (request.status === 0) {
+        return;
+      }
+
       const responseHeaders = parseHeaders(request.getAllResponseHeaders());
       const responseData = responseType !== 'text' ? request.response : request.responseText
-      const reponse: AxiosResponse = {
+      const response: AxiosResponse = {
         data: responseData,
         status: request.status,
         statusText: request.statusText,
@@ -29,7 +37,15 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
         config,
         request
       }
-      resolve(reponse)
+      handleResponse(response);
+    }
+
+    request.onerror = function handleError () {
+      reject(new Error('Network Error'));
+    }
+
+    request.ontimeout = function handleTimeout () {
+      reject(new Error(`Timeout of ${timeout} ms exceeded`));
     }
 
     Object.keys(headers).forEach(name => {
@@ -41,5 +57,13 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
     })
 
     request.send(data)
+
+    function handleResponse(response: AxiosResponse): void {
+      if (response.status >= 200 && response.status < 300) {
+        resolve(response);
+      } else {
+        reject(new Error(`Request failed with status code ${response.status}`));
+      }
+    }
   })
 }
