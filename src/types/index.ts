caf45235ab806464e6cfa6c830d2e013413817ba
Edward-1020 <iezzuyoung@gmail.com>
2019-07-18 23:38:43 +0800

    feat: clear code

--- src/types/index.ts
+++ src/types/index.ts
@@ -45,6 +45,11 @@ export interface AxiosError extends Error {
 }
 
 export interface Axios {
+  interceptors: {
+    request: AxiosInterceptorManager<AxiosRequestConfig>
+    response: AxiosInterceptorManager<AxiosResponse>
+  }
+
   request<T = any>(config: AxiosRequestConfig): AxiosPromise<T>
 
   get<T = any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
@@ -69,7 +74,7 @@ export interface AxiosInstance extends Axios {
 }
 
 export interface AxiosInterceptorManager<T> {
-  use(resolved: ResolvedFn<T>, rejected: RejectedFn): number;
+  use(resolved: ResolvedFn<T>, rejected?: RejectedFn): number;
 
   eject(id: number): void
 }
